b8ac08669409b677cefce409709a7475a3745850
crittericallyonline <ballstenny@gmail.com>
2026-05-29 03:02:12 -0700

    this is a small patch so you dont have to manually copy the `configtemplate.h -> config.h` manually

--- src/libresample/libresample.mk
+++ src/libresample/libresample.mk
@@ -9,6 +9,13 @@ HEADERS := $(shell find $(SRC) -name "*.h")
 DEFS := $(patsubst %.c, %.d, $(SOURCES))
 OBJECTS := $(patsubst %.c, %.o, $(SOURCES))
 INCLUDE=-Iinclude -I$(SRC)
+CONFIG_TEMPLATE_H=$(SRC)/configtemplate.h
+CONFIG_H=$(SRC)/config.h
+
+main: $(CONFIG_H) $(DEST)
+
+$(CONFIG_H): $(CONFIG_TEMPLATE_H)
+	@cp $< $@
 
 $(DEST): $(OBJECTS)
 	@emar rcs $@ $(OBJECTS)
