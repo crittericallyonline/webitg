b26eb10abe49d089f0a634825b9826198f8e340b
crittericallyonline <ballstenny@gmail.com>
2026-06-03 16:35:32 -0700

    why didint these get included wtf

--- build-wasm.sh
+++ build-wasm.sh
@@ -5,7 +5,7 @@ cd out
 emcmake cmake .. -B . -DCMAKE_TOOLCHAIN_FILE=/usr/lib/emscripten/cmake/Modules/Platform/Emscripten.cmake
 if emmake make -s -j8; then
     # emstrip --strip-unneeded -S WebITG.wasm
-    cp WebITG.html ../html
+    # cp WebITG.html ../html
     cp WebITG.js ../html
     cp WebITG.wasm ../html
 else
