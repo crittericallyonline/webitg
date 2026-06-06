39e858cf78d2156ae4848bb7b8697543ecaa73e1
crittericallyonline <ballstenny@gmail.com>
2026-06-05 23:26:58 -0700

    applying changes, going to bed

--- src/StepMania.cpp
+++ src/StepMania.cpp
@@ -1166,12 +1166,9 @@ int main(int argc, char* argv[])
 	/* depends on SONGINDEX: */
 	SONGMAN		= new SongManager();
 
-	LOG->Info("LoadingWindow File %s ----- Line: %d", __FILE__, __LINE__ + 1);
 	SONGMAN->InitAll( loading_window );		// this takes a long time
 
-	LOG->Info("CryptManager File %s ----- Line: %d", __FILE__, __LINE__ + 1);
 	CRYPTMAN	= new CryptManager;	// need to do this before ProfileMan
-	LOG->Info("MemoryCardmanager File %s ----- Line: %d", __FILE__, __LINE__ + 1);
 	MEMCARDMAN	= new MemoryCardManager;
 	PROFILEMAN	= new ProfileManager;
 	PROFILEMAN->Init();				// must load after SONGMAN
@@ -1191,18 +1188,14 @@ int main(int argc, char* argv[])
 	/* This shouldn't need to be here; if it's taking long enough that this is
 	 * even visible, we should be fixing it, not showing a progress display. */
 	SaveCatalogXml( loading_window );
-
-	LOG->Info("LOG NIKO 3 SETUP");
 	
 	NSMAN 		= new NetworkSyncManager( loading_window ); 
 	MESSAGEMAN	= new MessageManager;
 	STATSMAN	= new StatsManager;
-	LOG->Info("LOG NIKO 4 amaze");
 
 	SAFE_DELETE( loading_window );		// destroy this before init'ing Display
 
 	StartDisplay();
-	LOG->Info("LOG NIKO 5 DISPLAY SETUP");
 
 	StoreActualGraphicOptions( true );
 
