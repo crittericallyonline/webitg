8c27a9d484024348735c70509b408211622fbc2b
crittericallyonline <ballstenny@gmail.com>
2026-06-05 22:48:48 -0700

    we dont need PS2 themes lol

--- data/Themes/ps2onpc/Scripts/Other.lua
+++
@@ -1,6 +0,0 @@
--- This is a stub for the PS2.
-function LoadScreenIfControllersPresent( screen )
-	-- Comment out this line to test the "insert controller" screen on a PC.
-	SCREENMAN:SetNewScreen( screen )
-end
-
